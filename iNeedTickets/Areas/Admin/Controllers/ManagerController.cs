591124cb403c1ee68af6f8f57e58efd03d5359c2
Mihail Georgescu <geo.mihail96@gmail.com>
2020-04-04 08:12:23 +0300

    Started events management implementation, plus mobile css changes

---
+++ iNeedTickets/Areas/Admin/Controllers/ManagerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iNeedTickets.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iNeedTickets.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ManagerController : Controller
+    {
+        private IEventRepository _eventRepository;
+
+        public ManagerController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var eventsList = _eventRepository.GetAllUpcomingEvents();
+
+            return View("ManagerMenu", eventsList.ToList());
+        }
+    }
+}
