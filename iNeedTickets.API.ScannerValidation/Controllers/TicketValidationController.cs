e6171cd9046c5dd613b0c381d5895427441c784e
Mihail Georgescu <geo.mihail96@gmail.com>
2020-04-09 03:17:40 +0300

    Added WebAPI projects for mobile app integration

---
+++ iNeedTickets.API.ScannerValidation/Controllers/TicketValidationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iNeedTickets.API.ScannerValidation.Controllers
+{
+    [Route("api/validate")]
+    [ApiController]
+    public class TicketValidationController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Validation");
+        }
+    }
+}
---
+++ iNeedTickets.API/Controllers/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iNeedTickets.Models;
+using iNeedTickets.Repos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iNeedTickets.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("API");
+        }
+    }
+}
